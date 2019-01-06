d816f32b348e7cb9402f013e2dc7d31e143b296e
VijithaEkanayake <ekvijitha@gmail.com>
2019-01-06 18:46:59 +0530

    Add aws configuration files

---
+++ http/resiliency/provision-infrastructure.sh
@@ -0,0 +1,57 @@
+#!bin/bash
+
+output_dir=$2
+echo $output_dir
+
+cluster_name="ballerina-http-cluster"
+retry_attempts=3
+config_file=~/.kube/config
+echo $retry_attempts
+
+
+while [ "$STATUS" != "ACTIVE" ] && [ $retry_attempts -gt 0 ]
+do
+    eksctl create cluster --name "$cluster_name" --region us-east-1 --nodes-max 3 --nodes-min 1 --node-type t2.small --zones=us-east-1a,us-east-1b,us-east-1d
+    if [ $? -ne 0 ]; then
+         echo "Waiting for service role.."
+         aws cloudformation wait stack-create-complete --stack-name=EKS-$cluster_name-ServiceRole
+         echo "Waiting for vpc.."
+         aws cloudformation wait stack-create-complete --stack-name=EKS-$cluster_name-VPC
+         echo "Waiting for Control Plane.."
+         aws cloudformation wait stack-create-complete --stack-name=EKS-$cluster_name-ControlPlane
+         echo "Waiting for node-group.."
+         aws cloudformation wait stack-create-complete --stack-name=EKS-$cluster_name-DefaultNodeGroup
+    else
+        #if the cluster creation is succesful , any existing config files are removed
+        if [ -f "$config_file" ];then
+            rm $config_file
+        fi
+
+        #Configure the security group of nodes to allow traffic from outside
+        node_security_group=$(aws ec2 describe-security-groups --filter Name=tag:aws:cloudformation:logical-id,Values=NodeSecurityGroup --query="SecurityGroups[0].GroupId" --output=text)
+        aws ec2 authorize-security-group-ingress --group-id $node_security_group --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
+    fi
+    STATUS=$(aws eks describe-cluster --name $cluster_name --query="[cluster.status]" --output=text)
+    echo "Status is "$STATUS
+    retry_attempts=$(($retry_attempts-1))
+    echo "attempts left : "$retry_attempts
+done
+
+#if the status is not active by this phase the cluster creation has failed, hence exiting the script in error state
+if [ "$STATUS" != "ACTIVE" ];then
+    echo "state is not active"
+    exit 1
+fi
+
+# Check if config file exists, if it does not exist create the config file
+if [ ! -f "$config_file" ];then
+    echo "config file does not exist"
+    eksctl utils write-kubeconfig --name $cluster_name --region us-east-1
+fi
+
+infra_properties=$output_dir/infrastructure.properties
+
+# echo $kube_master
+# echo $output_dir
+# echo "KUBERNETES_MASTER=$kube_master" > $output_dir/k8s.properties
+echo "ClusterName=$cluster_name" >> $output_dir/infrastructure.properties
